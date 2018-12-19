2b9e7ad7765145247576a37add182c281a76b600
zhuxudong <callzhuxudong@163.com>
2018-12-19 11:59:57 +0800

    碰撞修正

--- src/page/scene/common.js
+++ src/page/scene/common.js
@@ -5,10 +5,14 @@ function random(min, max) {
 function collide($dom1, $dom2) {
     let rect1 = $dom1[0].getBoundingClientRect();
     let rect2 = $dom2[0].getBoundingClientRect();
-    return rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.height + rect1.y > rect2.y
+    rect1.width = rect1.width || rect1.right - rect1.left;
+    rect2.width = rect2.width || rect2.right - rect2.left;
+    rect1.height = rect1.height || rect1.bottom - rect1.top;
+    rect2.height = rect2.height || rect2.bottom - rect2.top;
+    return rect1.left < rect2.left + rect2.width &&
+        rect1.left + rect1.width > rect2.left &&
+        rect1.top < rect2.top + rect2.height &&
+        rect1.height + rect1.top > rect2.top
 }
 
 export {
